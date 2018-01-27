47bdd1bc33112446be142186c2ef8aa10e10bb84
monzag <zaganiacz.m@gmail.com>
2018-01-27 22:40:03 +0100

    Add removeTest

--- src/test/java/com/monzag/DoublyLinkedListTest.java
+++ src/test/java/com/monzag/DoublyLinkedListTest.java
@@ -1,6 +1,7 @@
 package com.monzag;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,9 +14,18 @@ class DoublyLinkedListTest {
         this.dbLinkedList = new DoublyLinkedList();
     }
 
+    @Test
+    void removeTest() {
+        addDataToLinkedList();
+        dbLinkedList.remove(2);
+        dbLinkedList.remove(8);
+        String expected = "0 1 3 4 5 6 7 8 10";
+        assertEquals(expected, dbLinkedList.toString());
+    }
+
     public void addDataToLinkedList() {
         for (int i = 0; i < 11; i++) {
-            dblinkedList.add(i);
+            dbLinkedList.add(i);
         }
     }
 
