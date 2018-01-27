28c7dae31ac54a2bcc44088e773785d8b8c4e07b
monzag <zaganiacz.m@gmail.com>
2018-01-27 22:31:20 +0100

    Create DoublyLinkedListTest class

---
+++ src/test/java/com/monzag/DoublyLinkedListTest.java
@@ -0,0 +1,16 @@
+package com.monzag;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DoublyLinkedListTest {
+
+    private DoublyLinkedList dbLinkedList;
+
+    @BeforeEach
+    void createLinkedList() {
+        this.dbLinkedList = new DoublyLinkedList();
+    }
+
+}
