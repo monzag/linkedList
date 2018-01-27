7cfeba431a4a18281ead36f9965e21457d7841e4
monzag <zaganiacz.m@gmail.com>
2018-01-27 22:29:39 +0100

    Add previous field, getter and setter

--- src/main/java/com/monzag/Node.java
+++ src/main/java/com/monzag/Node.java
@@ -4,15 +4,23 @@ public class Node {
 
     private Object data;
     private Node next;
+    private Node previous;
 
     public Node(Object data) {
         this.data = data;
         this.next = null;
+        this.previous = null;
     }
 
     public Node(Object data, Node next) {
         this.data = data;
         this.next = next;
+    }
+
+    public Node(Object data, Node next, Node previous) {
+        this.data = data;
+        this.next = next;
+        this.previous = previous;
     }
 
     public Node getNext() {
@@ -29,5 +37,13 @@ public class Node {
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+    public Node getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(Node previous) {
+        this.previous = previous;
     }
 }
