7a06b7d1d517f376d1c12717399c88b266a7e3c1
monzag <zaganiacz.m@gmail.com>
2018-01-27 22:39:38 +0100

    Add remove and add methods

--- src/main/java/com/monzag/DoublyLinkedList.java
+++ src/main/java/com/monzag/DoublyLinkedList.java
@@ -1,6 +1,34 @@
 package com.monzag;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList extends SinglyLinkedList {
 
-    
+    public DoublyLinkedList() {
+        super();
+    }
+
+    @Override
+    public void remove(int index) {
+        Node removed = get(index);
+        if (removed != null) {
+            removed.getPrevious().setNext(removed.getNext());
+            removed.getNext().setPrevious(removed.getPrevious());
+        }
+        length--;
+    }
+
+    @Override
+    public void add(Object item) {
+        Node node = new Node(item);
+
+        if (head == null) {
+            this.head = node;
+            this.last = this.head;
+
+        } else {
+            last.setNext(node);
+            node.setPrevious(last);
+            this.last = node;
+        }
+        length++;
+    }
 }
