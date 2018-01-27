64680e2b9a19ce5e3863b8abb09f7108ad48eb99
monzag <zaganiacz.m@gmail.com>
2018-01-27 12:55:53 +0100

    Create schedule of SinglyLinkedList

--- src/main/java/com/monzag/SinglyLinkedList.java
+++ src/main/java/com/monzag/SinglyLinkedList.java
@@ -1,4 +1,22 @@
 package com.monzag;
 
 public class SinglyLinkedList {
+
+    public Node head;
+    public int length;
+
+    public SinglyLinkedList() {
+    }
+
+    public void add(int item) {
+
+    }
+
+    public void insert(int index, int item) {
+
+    }
+
+    public void remove(int index) {
+
+    }
 }
