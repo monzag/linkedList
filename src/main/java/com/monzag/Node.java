41bf31cbf492eed847282e6f3006cbe560fb9377
monzag <zaganiacz.m@gmail.com>
2018-01-27 12:28:51 +0100

    Add Node class

---
+++ src/main/java/com/monzag/Node.java
@@ -0,0 +1,33 @@
+package com.monzag;
+
+public class Node {
+
+    private Object data;
+    private Node next;
+
+    public Node(Object data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Node(Object data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
