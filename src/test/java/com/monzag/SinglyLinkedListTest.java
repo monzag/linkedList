9d88ecde213a215e6c4a7b6d6e8120e9e3fb26ce
monzag <zaganiacz.m@gmail.com>
2018-01-27 15:28:41 +0100

    Add headTest

--- src/test/java/com/monzag/SinglyLinkedListTest.java
+++ src/test/java/com/monzag/SinglyLinkedListTest.java
@@ -26,6 +26,14 @@ class SinglyLinkedListTest {
     }
 
     @Test
+    void headTest() {
+        linkedList.add(0);
+        linkedList.add(1);
+        int expected = 0;
+        assertEquals(expected, linkedList.head.getData());
+    }
+    
+    @Test
     void addTest() {
         linkedList.add(0);
         linkedList.add(1);
