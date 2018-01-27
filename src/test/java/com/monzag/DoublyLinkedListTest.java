71f94c1c0b6541ae6b6b24be3b4b60bb8ea36f99
monzag <zaganiacz.m@gmail.com>
2018-01-27 23:04:17 +0100

    Add insertToEndTest

--- src/test/java/com/monzag/DoublyLinkedListTest.java
+++ src/test/java/com/monzag/DoublyLinkedListTest.java
@@ -32,6 +32,14 @@ class DoublyLinkedListTest {
         assertEquals(expected, dbLinkedList.toString());
     }
 
+    @Test
+    void insertToEndTest() {
+        addDataToLinkedList();
+        dbLinkedList.insert(11, 11);
+        dbLinkedList.insert(50, 50);
+        String expected = "0 1 2 3 4 5 6 7 8 9 10 11 50";
+        assertEquals(expected, dbLinkedList.toString());
+    }
 
     public void addDataToLinkedList() {
         for (int i = 0; i < 11; i++) {
